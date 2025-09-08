952a11c8c5eb0e9d286902f6d4740f8a1c15151d
kirill pavlov <2020kirillp@gmail.com>
2025-09-08 13:28:24 -0700

    Initial Commit

---
+++ RepTimer/RepTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepTimerApp.swift
+//  RepTimer
+//
+//  Created by Kirill Pavlov on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RepTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RepTimerTests/RepTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RepTimerTests.swift
+//  RepTimerTests
+//
+//  Created by Kirill Pavlov on 9/8/25.
+//
+
+import Testing
+@testable import RepTimer
+
+struct RepTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
